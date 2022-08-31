29217c1fdd147c094f6eba79393966cb320b0c4f
Siddhartha-Padhy <siddhartha6222@gmail.com>
2022-08-31 22:53:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css'
+import Row from './components/Row'
+import requests from './requests'
+
+function App() {
+  return (
+    <div style={{ backgroundColor: '#0f0f0f' }}>
+      Hello there!
+      <Row
+        title={'Netflix Orignals'}
+        large={true}
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row
+        title={'Trending Now'}
+        large={false}
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row
+        title={'Top Rated'}
+        large={false}
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row
+        title={'Action Movies'}
+        large={false}
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title={'Comedy Movies'}
+        large={false}
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title={'Horror Movies'}
+        large={false}
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title={'Romance Movies'}
+        large={false}
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title={'Documentaries'}
+        large={false}
+        fetchUrl={requests.fetchDocumantaries}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Row.jsx
@@ -0,0 +1,46 @@
+import axios from '../axios'
+import React, { useState, useEffect } from 'react'
+
+const base_url = 'https://image.tmdb.org/t/p/original/'
+
+function Row({ title, large, fetchUrl }) {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results)
+      console.log(request.data)
+    }
+    fetchMovies()
+  }, [fetchUrl])
+
+  return (
+    <div className="my-1 mx-2 p-1">
+      <h2
+        className="text-white"
+        style={{ fontFamily: "'Roboto', sans-serif", fontSize: '1.3rem' }}
+      >
+        {title}
+      </h2>
+      <div className="flex flex-row my-2 p-2 overflow-x-scroll overflow-y-hidden posters">
+        {movies?.map((movie, index) => {
+          return (
+            <img
+              key={index}
+              src={base_url + movie.poster_path}
+              className={
+                large
+                  ? 'object-contain w-full mr-2.5 hover:scale-110 duration-300 max-h-32'
+                  : 'object-contain w-full mr-2.5 hover:scale-110 duration-300 max-h-24'
+              }
+              alt={movie.name}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Row
